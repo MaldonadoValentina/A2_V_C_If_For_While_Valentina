d077dcfb052d8cb9879870bf488985822d305adb
MaldonadoValentina <a.22308051281253@cbtis128.edu.mx>
2024-09-06 09:35:01 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+'''Valentina Maldonado
+Trabajando con cadenas
+'''
+nombre = "Valentina"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["V", "A", "L", "E"]
+for chicas in MisNovias:
+        print(chicas)
+
+        print("Otro ejemplo")
+        b = "Valentina Maldonado"
+print(b[2:5]) #Consigue los caracteres de la posición 2 a la posición 5 (Distancia de 3)
+print(" a mayusculas")
+a = "valentina maldonado"
+print(a.upper())
+print("Concatenar") #Conecta cadenas
+a = "Maria"
+b = "Perez"
+c = a + "_" + b
+print(c)
---
+++ misvariables.py
@@ -0,0 +1,16 @@
+import math
+edad = 17 # 17 se guarda en edad
+
+nombre = "Valentina Maldonado" 
+print("Tu edad es:", edad)
+print("Tu nombre es:", nombre)
+print("--Tipo de Variables---")
+x = str(nombre) 
+y = int(3.1415)
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("--Funciones Matematicas----")
+x = math.sqrt(64)
+print(x)
